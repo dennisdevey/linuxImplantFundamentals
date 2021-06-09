55ad16a986c1cc33a5f9ea7f3795ab8491b1c384
dennisdevey <84914865+dennisdevey@users.noreply.github.com>
2021-06-09 14:35:08 -0400

    Update profilerDemo.c

--- resources/misc/profilerDemo.c
+++ resources/misc/profilerDemo.c
@@ -1,21 +1,35 @@
+/* This makes a big ole string of // separated fields that could be useful for profiling. 
+Easier than that, if you want to use this as a validator, you can just check to see that values 
+are equal to what you want them to be and return true / false */
+
+// an example of this is located with UTS_SYSNAME
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/utsname.h>
 #include <unistd.h>
 #include <string.h>
-
+#define UTS_SYSNAME "Linux"
 
 int main( int argc, char *argv[] )
 {
-  char * giantList = malloc(1024);
+  
   struct utsname uts;
   uname(&uts);
+#ifdef UTS_SYSNAME
+  if (strcmp(uts.sysname, UTS_SYSNAME)==0){
+    return 0;
+  else {
+    return -1;
+  }
+#endif
 #ifdef DEBUG
   printf("%s:%s\n",uts.sysname, uts.machine);
   printf("%s\n",uts.release);
   printf("%s\n",uts.version);
 #endif
-
+#ifdef STRING
+  char * giantList = malloc(1024);
   strcat(giantList,uts.sysname);
   strcat(giantList,"//");
 
@@ -27,6 +41,7 @@ int main( int argc, char *argv[] )
 
   strcat(giantList,uts.version);
   strcat(giantList,"//");
+#endif
 
   FILE *fp;
   char path[1035];
@@ -37,9 +52,10 @@ int main( int argc, char *argv[] )
 #ifdef DEBUG
   printf("%s", path);
 #endif
+#ifdef STRING
   strcat(giantList, path);
   strcat(giantList,"//");
-
+#endif
   
   
   fp = popen("ldd --version", "r");
@@ -48,20 +64,23 @@ int main( int argc, char *argv[] )
 #ifdef DEBUG
   printf("%s", path);
 #endif
+#ifdef STRING
   strcat(giantList, path);
   strcat(giantList,"//");
-
+#endif
   fp = popen("localectl status", "r");
 
   fgets(path, sizeof(path), fp);
 #ifdef DEBUG
   printf("%s", path);
 #endif
+#ifdef STRING
   strcat(giantList, path);
   strcat(giantList,"//");
-
+#endif
+#ifdef DEBUG
   printf("%s", giantList); 
-
+#endif
   /* close */
   pclose(fp);
 
