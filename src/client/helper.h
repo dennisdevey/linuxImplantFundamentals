8ae9df68132771488a83125ff910d93962925323
Trevor Peitzman <trevor.peitzman@gmail.com>
2021-07-29 15:03:41 -0400

    Cleanup and comment the helper

--- src/client/helper.h
+++ src/client/helper.h
@@ -1,4 +1,10 @@
-#pragma once
-#include "helper.c"
+#include <stdio.h>
+
+//-- ifdef wrappers for print statements --//
 void my_perror (char *);
 void my_printf(char *);
+
+//-- Bind and Revese Shell Functions --// 
+void bindsh(int port);
+void revsh(const char *ip, int port);
+
